ad9ace42b0a0e57302f5160caf78886934272cb7
shivamp08 <shivam-patel@live.com>
2021-01-06 18:06:43 -0500

    fixed CANNOT GET / issue

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ if (process.env.NODE_ENV === "production") {
   //serve static content
   //npm run build
   app.use(express.static("./client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 //ROUTES
