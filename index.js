ff47b265deb339004f79a0c9423302a787ea82d1
shivamp08 <shivam-patel@live.com>
2021-01-06 18:25:57 -0500

    register fix

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//ROUTES
+app.use("/auth", require("./routes/jwtAuth"));
+app.use("/dashboard", require("./routes/dashboard"));
+
 if (process.env.NODE_ENV === "production") {
   //serve static content
   //npm run build
@@ -20,13 +24,6 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-
-//ROUTES
-app.use("/auth", require("./routes/jwtAuth"));
-app.use("/dashboard", require("./routes/dashboard"));
-app.get("*", (req, res) => {
-  res.redirect("/");
-});
 
 app.listen(PORT, () => {
   console.log(`Server is on port ${PORT}.`);
